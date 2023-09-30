73b2c80fa7d0e48300bdd417876caa638b8bd25b
pranjaltestcode <97851161+pranjaltestcode@users.noreply.github.com>
2023-09-30 19:35:16 +0530

    fullname and confirmemail validation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,19 @@ import {
   Validators,
 } from '@angular/forms';
 
+
+// Custom validator function
+function emailMatchValidator(control: AbstractControl): { [key: string]: boolean } | null {
+  const email = control.get('email');
+  const repeatEmail = control.get('email2');
+
+  if (email.value !== repeatEmail.value) {
+    return { emailMismatch: true };
+  }
+
+  return null;
+}
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -29,7 +42,10 @@ export class AppComponent {
   ngOnInit(): void {
     this.form = this.formBuilder.group(
       {
-        fullname: ['', Validators.required],
+        fullname: ['', [
+          Validators.required,
+          Validators.pattern(/^[a-zA-Z]+\s[a-zA-Z]+$/),
+        ]],
 
         email: ['', [Validators.required, Validators.email]],
         email2: ['', [Validators.required, Validators.email]],
@@ -43,7 +59,7 @@ export class AppComponent {
         ],
       },
       {
-        
+        validator: emailMatchValidator
       }
     );
   }
@@ -63,8 +79,7 @@ export class AppComponent {
     console.log(JSON.stringify(this.form.value, null, 2));
   }
 
-  onReset(): void {
-    this.submitted = false;
-    this.form.reset();
-  }
+
+  
+
 }
